b563c615b035a343a4a0d9fdf45a5286808899d5
LMRomax <maxencectmd.lemaitre@gmail.com>
2021-02-26 15:36:04 +0100

    animation accordion toggle corrected

--- public/js/accordionFaq.js
+++ public/js/accordionFaq.js
@@ -5,20 +5,21 @@ for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         for (var i = 0; i < acc.length; i++) {
             acc[i].classList.remove('active');
+            if(this !== acc[i]) {
+                var panel = acc[i].nextElementSibling;
+                panel.style.display = "none";
+            }
         }
         
         this.classList.add("active");
-
-        for (var i = 0; i < acc.length; i++) {
-            var panel = acc[i].nextElementSibling;
-            panel.style.display = "none";
-        }
 
         var panel = this.nextElementSibling;
 
         if (panel.style.display === "block") {
             panel.style.display = "none";
-        } else {
+        } else if(panel.style.display === "none") {
+            panel.style.display = "block";
+        } else if(panel.style.display === "") {
             panel.style.display = "block";
         }
     });
